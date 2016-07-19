6d9f9396a38df0ea64370a47c09334ad8716c298
Srijita Jana <jana.srijita@gmail.com>
2016-07-19 01:22:29 -0600

    Update plot_code.r

--- plot_code.r
+++ plot_code.r
@@ -8,7 +8,6 @@ salmondata <- read.csv("data.csv",header = TRUE, check.names=TRUE)
 
 ## convert from wide to long format
 data_long <- gather(salmondata, Year, Count, X1993:X2013)
-
 data_long$Year <- as.numeric(substring(data_long$Year, 2))
 
 # replace 0 with NAs
@@ -17,8 +16,7 @@ colnames(data_long)[1] <- "Julian"
 
 ## plot data as is
 brks <- c(1993:2013)
-labs <- brks
 ggplot(data_long, aes(Year, Julian, size = factor(Count))) +
   geom_point() + theme_bw() + scale_size_discrete(name="Salmon\nCount") +
   scale_x_continuous(breaks=brks,
-                   labels=labs)
+                   labels=brks)
